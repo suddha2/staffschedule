4a502c979bf645613bef6f522fecce79a532aee2
suddha2 <suddha@gmail.com>
2025-09-01 02:50:44 +0100

    added Shift repository

---
+++ src/main/java/com/midco/rota/repository/ShiftRepository.java
@@ -0,0 +1,21 @@
+package com.midco.rota.repository;
+
+import java.time.LocalDate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.midco.rota.model.Shift;
+import com.midco.rota.model.ShiftTemplate;
+
+public interface ShiftRepository extends JpaRepository<Shift, Integer>  {
+
+//	Shift findByShiftTemplateAndStartTime(ShiftTemplate template, LocalDate date);
+	
+	@Query("SELECT s FROM Shift s WHERE s.shiftTemplate = :template AND s.shiftStart = :date")
+	Shift findByShiftTemplateAndStartTime(@Param("template") ShiftTemplate template,
+	                                      @Param("date") LocalDate date);
+
+
+}
